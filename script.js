57c2f121461f3649ea112eaf32e91c62f38abc28
Wojciech <gf1200@gmail.com>
2019-05-15 21:47:41 +0200

    added redux library and implemented click event to increase initial number

--- script.js
+++ script.js
@@ -9,16 +9,28 @@ const counter = (state = 0, action) => {
 	}
 };
 
-expect(counter(0, { type: 'INCREMENT' })).toEqual(1);
+const { createStore } = Redux;
+const store = createStore(counter);
 
-expect(counter(1, { type: 'INCREMENT' })).toEqual(2);
+const render = () => {
+	document.body.innerHTML = `<h1>${store.getState()}</h1>`;
+};
 
-expect(counter(2, { type: 'DECREMENT' })).toEqual(1);
+store.subscribe(render);
+render();
 
-expect(counter(1, { type: 'DECREMENT' })).toEqual(0);
+document.addEventListener('click', () => store.dispatch({ type: 'INCREMENT' }));
 
-expect(counter(1, { type: 'SOMETHING_ELSE' })).toEqual(1);
+// expect(counter(0, { type: 'INCREMENT' })).toEqual(1);
 
-expect(counter(undefined, {})).toEqual(0);
+// expect(counter(1, { type: 'INCREMENT' })).toEqual(2);
 
-console.log('Tests passed.');
+// expect(counter(2, { type: 'DECREMENT' })).toEqual(1);
+
+// expect(counter(1, { type: 'DECREMENT' })).toEqual(0);
+
+// expect(counter(1, { type: 'SOMETHING_ELSE' })).toEqual(1);
+
+// expect(counter(undefined, {})).toEqual(0);
+
+// console.log('Tests passed.');
