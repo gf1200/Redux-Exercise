15c95a9513c7cb63b2b8c501c76392d49416279f
Wojciech <gf1200@gmail.com>
2019-05-16 15:05:13 +0200

    added reducer with composition

--- script.jsx
+++ script.jsx
@@ -1,75 +1,68 @@
-const counter = (state = [0], action) => {
+const toDo = (state, action) => {
 	switch (action.type) {
-		case 'INCREMENT':
-			return [...state.slice(0, action.id), state[action.id] + 1, ...state.slice(action.id + 1)];
-		case 'DECREMENT':
-			return [...state.slice(0, action.id), state[action.id] - 1, ...state.slice(action.id + 1)];
-		case 'ADD_COUNTER':
-			return state.concat([0]);
-		case 'REMOVE_COUNTER':
-			return [...state.slice(0, action.id), ...state.slice(action.id + 1)];
+		case 'ADD_TODO':
+			return {
+				id: action.id,
+				text: action.text,
+				completed: false
+			};
+		case 'TOGGLE_TODO':
+			if (state.id !== action.id) return state;
+			return {
+				...state,
+				completed: !state.completed
+			};
 		default:
 			return state;
 	}
 };
 
-const incrementCounter = (list, index) =>
-	list
-		.slice(0, index)
-		.concat(list[index] + 1)
-		.concat(list.slice(index + 1));
+const toDos = (state = [], action) => {
+	switch (action.type) {
+		case 'ADD_TODO':
+			return [...state, todo(state, action)];
+		case 'TOGGLE_TODO':
+			return state.map(todo => toDo(todo, action));
 
-const { createStore } = Redux;
-
-const store = createStore(counter);
-
-const Counter = ({ value, onIncrement, removeCounter, onDecrement }) => (
-	<div>
-		<h1>{value}</h1>
-		<button onClick={onIncrement}>+</button>
-		<button onClick={onDecrement}>-</button>
-		<button onClick={removeCounter}>delete</button>
-	</div>
-);
-
-const CountersList = ({ list, addCounter }) => (
-	<div>
-		<button onClick={addCounter}>Add new counter</button>
-		{list.map((counter, index) => (
-			<Counter
-				key={index}
-				value={counter}
-				onIncrement={() => store.dispatch({ type: 'INCREMENT', id: index })}
-				removeCounter={() => store.dispatch({ type: 'REMOVE_COUNTER', id: index })}
-				onDecrement={() => store.dispatch({ type: 'DECREMENT', id: index })}
-			/>
-		))}
-	</div>
-);
-
-const render = () => {
-	ReactDOM.render(
-		<CountersList
-			list={store.getState()}
-			addCounter={() => store.dispatch({ type: 'ADD_COUNTER' })}
-		/>,
-		document.getElementById('root')
-	);
+		default:
+			return state;
+	}
 };
 
-store.subscribe(render);
-render();
+//TESTS
 
-// expect(counter(0, { type: 'INCREMENT' })).toEqual(1);
+const testToggleTodo = () => {
+	const stateBefore = [
+		{
+			id: 0,
+			text: 'Learn Redux',
+			completed: false
+		},
+		{
+			id: 1,
+			text: 'Go shopping',
+			completed: false
+		}
+	];
+	const action = {
+		type: 'TOGGLE_TODO',
+		id: 1
+	};
+	const stateAfter = [
+		{
+			id: 0,
+			text: 'Learn Redux',
+			completed: false
+		},
+		{
+			id: 1,
+			text: 'Go shopping',
+			completed: true
+		}
+	];
 
-// expect(counter(1, { type: 'INCREMENT' })).toEqual(2);
+	expect(toDos(stateBefore, action)).toEqual(stateAfter);
+};
 
-// expect(counter(2, { type: 'DECREMENT' })).toEqual(1);
-
-// expect(counter(1, { type: 'DECREMENT' })).toEqual(0);
-
-// expect(counter(1, { type: 'SOMETHING_ELSE' })).toEqual(1);
-
-// expect(counter(undefined, {})).toEqual(0);
-
-// console.log('Tests passed.');
+testToggleTodo();
+console.log('All tests passed.');
